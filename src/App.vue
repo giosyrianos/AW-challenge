3715e7ded0c2ade1f6f918f59deae5a9d798ba1c
gio <giosyrianos@gmail.com>
2022-11-11 19:18:06 +0200

    chore: first commit after clean up

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  h1 {
+    color: red;
+  }
+}
+
+@media (min-width: 1024px) {
+}
+</style>
