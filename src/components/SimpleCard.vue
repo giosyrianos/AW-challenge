6b47c0b60e8391728f0200ed450fee7c30f45990
gio <giosyrianos@gmail.com>
2022-11-11 23:20:58 +0200

    feat: setup responsive layout

--- src/components/SimpleCard.vue
+++ src/components/SimpleCard.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 
 <template>
-  <div class="card bg-secondary bg-gradient p-3" style="width: 18rem">
+  <div class="card bg-secondary bg-gradient p-1">
     <div class="card-body">
-      <h5 class="card-title ">Card title</h5>
+      <h5 class="card-title">Card title</h5>
       <p class="card-text">
         Some quick example text to build on the card title and make up the bulk
         of the card's content.
@@ -14,3 +14,9 @@
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.card {
+  // max-width: 250px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,43 @@ import SimpleCard from "@/components/SimpleCard.vue";
       <div class="row mb-3">
         <h4 class="secondary">Latest Posts</h4>
         <div class="cards-container">
-          <SimpleCard v-for="index in 10" :key="index" />
+          <SimpleCard v-for="index in 4" :key="index" />
+        </div>
+        <div class="row">
+          <nav
+            class="d-flex justify-content-center mt-2"
+            aria-label="Page navigation example"
+          >
+            <ul class="pagination">
+              <li class="page-item">
+                <a
+                  class="page-link bg-secondary text-light"
+                  href="#"
+                  aria-label="Previous"
+                >
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link bg-secondary text-light" href="#">1</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link bg-secondary text-light" href="#">2</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link bg-secondary text-light" href="#">3</a>
+              </li>
+              <li class="page-item">
+                <a
+                  class="page-link bg-secondary text-light"
+                  href="#"
+                  aria-label="Next"
+                >
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
       <div class="row">
@@ -23,6 +59,18 @@ import SimpleCard from "@/components/SimpleCard.vue";
 .cards-container {
   display: flex;
   flex-wrap: wrap;
-  gap: 1rem;
+  justify-content: center;
+	gap: 1.2rem;
+}
+
+@media (min-width: 760px) {
+  .cards-container {
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+    height: 30vh;
+    overflow-y: auto;
+    padding: 1rem;
+  }
 }
 </style>
